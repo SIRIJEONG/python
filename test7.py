80ab6c85369580d0eba2911b8d4fa40da54bd5a5
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2022-08-09 01:32:04 +0900

    test

---
+++ test7.py
@@ -0,0 +1,25 @@
+a = "hobby"
+print(a.count('b'))
+# hobby의 들어간 b의 갯수 찾기
+
+b = "apple"
+print(a.find('p'))
+# apple 에서 p가 언제 몇번째에 있는지 찾기 만약 없는 값을 찾으라고 입력하면 -1이 나온다.
+
+c = ",".join("abcd")
+print(c)
+
+d = "hi"
+print(d.upper())
+# 소문자를 대문자로 변경 
+e = "HI"
+print(e.lower())
+# 대문자를 소문자로 변경 
+
+f = "show me the money"
+print(f.replace("money", "book"))
+# 문자를 바꾸고 싶을때 사용한다.
+
+g = "show me the money"
+print(g.split())
+# 문자열 자료형이있으면 띄어씌기를 기준으로 리스트를 만든다.
